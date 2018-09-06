53961546acdcdc493b0ad343358d76e1671f0fe7
lingling8498 <Ling.Wang@WolfDen.wolfmedical.com>
2018-09-06 14:38:48 -0700

    Angular IO Tutorial Guide - DisplayData - May 21 2018

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: `
+    <h1> {{ title }} </h1>
+    <h2>My favorite hero is: {{ myHero.name }}</h2>
+    <p *ngIf="heroes.length > 3">There are many heroes!</p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{ hero.name }}
+      </li>
+    </ul>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public title: string;
+  public myHero: string;
+  public heroes = [];
+
+  constructor(){
+    this.title = 'Tour of Heroes';
+    
+    //this.heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+    this.heroes = [
+      new Hero(1, 'Windstorm'),
+      new Hero(2, 'Bombasto'),
+      new Hero(3, 'Magneta'),
+      new Hero(4, 'Tornado'),
+    ]
+    this.myHero = this.heroes[0];
+  }
 }
